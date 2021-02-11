deaa70a4581f12f068639d9c6fb321238cf4a586
darivanhatUW <78838344+darivanhatUW@users.noreply.github.com>
2021-02-10 21:48:39 -0800

    Add files via upload

--- Assigment05_Starter.py
+++ Assigment05_Starter.py
@@ -11,19 +11,29 @@
 
 # -- Data -- #
 # declare variables and constants
-objFile = "ToDoList.txt"   # An object that represents a file
+objFile = None  # An object that represents a file
+strFile = "ToDoList.txt"
 strData = ""  # A row of text data from the file
 dicRow = {}    # A row of data separated into elements of a dictionary {Task,Priority}
 lstTable = []  # A list that acts as a 'table' of rows
 strMenu = ""   # A menu of user options
 strChoice = "" # A Capture the user option selection
-dictRow1 = {"Task": "something", "Priority": "something else"}
-dictRow2 = {"Task": "something", "Priority": "something else"}
+
 
 # -- Processing -- #
-# Step 1 - When the program starts, load the any data you have
+# Step 1 - When the program starts, load any data you have
 # in a text file called ToDoList.txt into a python list of dictionaries rows (like Lab 5-2)
 # TODO: Add Code Here
+
+objFile = open(strFile, "w")
+dicRow = {"Task": "Vacuum", "Priority": "High"}
+#objFile.write(dicRow["Task"] + " | " + dicRow["Priority"] + "\n")
+lstTable.append(dicRow)
+dicRow = {"Task": "Dust", "Priority": "Low"}
+#objFile.write(dicRow["Task"] + " | " + dicRow["Priority" + "\n"])
+lstTable.append(dicRow)
+objFile.close()
+
 
 # -- Input/Output -- #
 # Step 2 - Display a menu of choices to the user
@@ -41,18 +51,29 @@ while (True):
     # Step 3 - Show the current items in the table
     if (strChoice.strip() == '1'):
         # TODO: Add Code Here
+        for row in lstTable:
+            print(row["Task"] + " | " + row["Priority"])
         continue
     # Step 4 - Add a new item to the list/Table
     elif (strChoice.strip() == '2'):
         # TODO: Add Code Here
+        dicRow = {"Task": "Wash Dishes", "Priority": "Medium"}
+        lstTable.append(dicRow)
         continue
     # Step 5 - Remove a new item from the list/Table
     elif (strChoice.strip() == '3'):
         # TODO: Add Code Here
+        lstTable.pop()
+        for row in lstTable:
+            print(row["Task"] + ", " + row["Priority"])
         continue
-    # Step 6 - Save tasks to the ToDoToDoList.txt file
+    # Step 6 - Save tasks to the ToDoList.txt file
     elif (strChoice.strip() == '4'):
         # TODO: Add Code Here
+        objFile = open(strFile, "a")
+        for row in lstTable:
+            objFile.write(row["Task"] + ", " + row["Priority"] + "\n")
+        objFile.close()
         continue
     # Step 7 - Exit program
     elif (strChoice.strip() == '5'):
